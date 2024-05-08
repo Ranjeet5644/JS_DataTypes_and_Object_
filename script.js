2a6d62046850b5ae20d71070b40cd788b8d11120
Ranjeet Kumar Yadav <138971007+Ranjeet5644@users.noreply.github.com>
2024-05-08 20:48:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hey this is tutorial 55");
+
+let a = 5;
+let b = 6;
+let c= "Harry";
+console.log(a+b+4);
+console.log (typeof a , typeof b , typeof c);
+
+//const b = 20;
+//b = b+2; //not allowed
+
+{
+    let a = 44
+    console.log(a)
+
+}
+    console.log(a);
+
+    let x = "Ranjeet";
+    let y = 22;
+    let z = 3.45;
+    const p = true;
+    let q = undefined;
+    let r= null;
+    console.log(x , y , z , p , q , r);
+    console.log(typeof x , typeof y , typeof z , typeof p , typeof q , typeof r);
+
+    // object
+
+    let o = {
+
+     Name : "Ranjeet",
+     "Job Code": 4500 ,
+      "is_handsome":true
+    }
+
+    console.log(o)
+    o.salary="10cr"
+
+    console.log(o)
+    o.salary="100cr"
+    console.log(0);
